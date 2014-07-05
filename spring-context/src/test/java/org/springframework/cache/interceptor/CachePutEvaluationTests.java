snicoll/spring-framework

commit eea230f2a8b31ee9d8248d97088d88a0ebe2be61

Allow @CachePut 'key' SpEL to refer to #result
Allow the @CachePut 'key' SpEL to refer to the result of the method call
via a '#result' variable.

This change is particularly useful when working with JPA entities that
have generated @Id values since the ID will often not be available
until the entity has been saved.

Issue: SPR-10664

spring-context/src/main/java/org/springframework/cache/interceptor/CacheAspectSupport.java
@@ -148,15 +148,15 @@ public abstract class CacheAspectSupport implements InitializingBean {
 		// Process any early evictions
 		processCacheEvicts(contexts.get(CacheEvictOperation.class), true, ExpressionEvaluator.NO_RESULT);
 
-		// Collect puts, either explicit @CachePuts or from a @Cachable miss
+		// Collect puts from any @Cachable miss
 		List<CachePutRequest> cachePutRequests = new ArrayList<CachePutRequest>();
-		collectPutRequests(contexts.get(CachePutOperation.class), cachePutRequests, false);
-		collectPutRequests(contexts.get(CacheableOperation.class), cachePutRequests, true);
+		collectPutRequests(contexts.get(CacheableOperation.class),
+				ExpressionEvaluator.NO_RESULT, cachePutRequests, true);
 
 		ValueWrapper result = null;
 
 		// We only attempt to get a cached result if there are no put requests
-		if(cachePutRequests.isEmpty()) {
+		if(cachePutRequests.isEmpty() && contexts.get(CachePutOperation.class).isEmpty()) {
 			result = findCachedResult(contexts.get(CacheableOperation.class));
 		}
 
@@ -164,6 +164,10 @@ public abstract class CacheAspectSupport implements InitializingBean {
 		if(result == null) {
 			result = new SimpleValueWrapper(invoker.invoke());
 		}
+
+		// Collect any explicit @CachePuts
+		collectPutRequests(contexts.get(CachePutOperation.class), result.get(),
+				cachePutRequests, false);
 
 		// Process any collected put requests, either from @CachePut or a @Cacheable miss
 		for (CachePutRequest cachePutRequest : cachePutRequests) {
@@ -182,13 +186,13 @@ public abstract class CacheAspectSupport implements InitializingBean {
 			CacheEvictOperation operation = (CacheEvictOperation) context.operation;
 			if (beforeInvocation == operation.isBeforeInvocation() &&
 					isConditionPassing(context, result)) {
-				performCacheEvict(context, operation);
+				performCacheEvict(context, operation, result);
 			}
 		}
 	}
 
 	private void performCacheEvict(CacheOperationContext context,
-			CacheEvictOperation operation) {
+			CacheEvictOperation operation, Object result) {
 		Object key = null;
 		for (Cache cache : context.getCaches()) {
 			if (operation.isCacheWide()) {
@@ -196,7 +200,7 @@ public abstract class CacheAspectSupport implements InitializingBean {
 				cache.clear();
 			} else {
 				if(key == null) {
-					key = context.generateKey();
+					key = context.generateKey(result);
 				}
 				logInvalidating(context, operation, key);
 				cache.evict(key);
@@ -213,10 +217,11 @@ public abstract class CacheAspectSupport implements InitializingBean {
 		}
 	}
 
-	private void collectPutRequests(Collection<CacheOperationContext> contexts, Collection<CachePutRequest> putRequests, boolean whenNotInCache) {
+	private void collectPutRequests(Collection<CacheOperationContext> contexts,
+			Object result, Collection<CachePutRequest> putRequests, boolean whenNotInCache) {
 		for (CacheOperationContext context : contexts) {
-			if (isConditionPassing(context, ExpressionEvaluator.NO_RESULT)) {
-				Object key = generateKey(context);
+			if (isConditionPassing(context, result)) {
+				Object key = generateKey(context, result);
 				if (!whenNotInCache || findInCaches(context, key) == null) {
 					putRequests.add(new CachePutRequest(context, key));
 				}
@@ -229,7 +234,8 @@ public abstract class CacheAspectSupport implements InitializingBean {
 		for (CacheOperationContext context : contexts) {
 			if (isConditionPassing(context, ExpressionEvaluator.NO_RESULT)) {
 				if(result == null) {
-					result = findInCaches(context, generateKey(context));
+					result = findInCaches(context,
+							generateKey(context, ExpressionEvaluator.NO_RESULT));
 				}
 			}
 		}
@@ -254,8 +260,8 @@ public abstract class CacheAspectSupport implements InitializingBean {
 		return passing;
 	}
 
-	private Object generateKey(CacheOperationContext context) {
-		Object key = context.generateKey();
+	private Object generateKey(CacheOperationContext context, Object result) {
+		Object key = context.generateKey(result);
 		Assert.notNull(key, "Null key returned for cache operation (maybe you "
 				+ "are using named params on classes without debug info?) "
 				+ context.operation);
@@ -393,9 +399,9 @@ public abstract class CacheAspectSupport implements InitializingBean {
 		 * Computes the key for the given caching operation.
 		 * @return generated key (null if none can be generated)
 		 */
-		protected Object generateKey() {
+		protected Object generateKey(Object result) {
 			if (StringUtils.hasText(this.operation.getKey())) {
-				EvaluationContext evaluationContext = createEvaluationContext(ExpressionEvaluator.NO_RESULT);
+				EvaluationContext evaluationContext = createEvaluationContext(result);
 				return CacheAspectSupport.this.evaluator.key(this.operation.getKey(), this.method, evaluationContext);
 			}
 			return CacheAspectSupport.this.keyGenerator.generate(this.target, this.method, this.args);

spring-aspects/src/test/java/org/springframework/cache/config/AnnotatedClassCacheableService.java
@@ -163,4 +163,11 @@ public class AnnotatedClassCacheableService implements CacheableService<Object> 
 	public Object multiUpdate(Object arg1) {
 		return arg1;
 	}
+
+	@Override
+	@CachePut(value="primary", key="#result.id")
+	public TestEntity putRefersToResult(TestEntity arg1) {
+		arg1.setId(Long.MIN_VALUE);
+		return arg1;
+	}
 }

spring-aspects/src/test/java/org/springframework/cache/config/CacheableService.java
@@ -70,4 +70,6 @@ public interface CacheableService<T> {
 	T multiConditionalCacheAndEvict(Object arg1);
 
 	T multiUpdate(Object arg1);
+
+	TestEntity putRefersToResult(TestEntity arg1);
 }

spring-aspects/src/test/java/org/springframework/cache/config/DefaultCacheableService.java
@@ -171,4 +171,11 @@ public class DefaultCacheableService implements CacheableService<Long> {
 	public Long multiUpdate(Object arg1) {
 		return Long.valueOf(arg1.toString());
 	}
+
+	@Override
+	@CachePut(value="primary", key="#result.id")
+	public TestEntity putRefersToResult(TestEntity arg1) {
+		arg1.setId(Long.MIN_VALUE);
+		return arg1;
+	}
 }

spring-aspects/src/test/java/org/springframework/cache/config/TestEntity.java
@@ -1,0 +1,56 @@
+/*
+ * Copyright 2002-2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cache.config;
+
+import org.springframework.util.ObjectUtils;
+
+/**
+ * Simple test entity for use with caching tests.
+ *
+ * @author Michael Plšd
+ */
+public class TestEntity {
+
+	private Long id;
+
+	public Long getId() {
+		return this.id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	@Override
+	public int hashCode() {
+		return ObjectUtils.nullSafeHashCode(this.id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == this) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (obj instanceof TestEntity) {
+			return ObjectUtils.nullSafeEquals(this.id, ((TestEntity) obj).id);
+		}
+		return false;
+	}
+}

spring-context/src/test/java/org/springframework/cache/config/AbstractAnnotationTests.java
@@ -367,6 +367,16 @@ public abstract class AbstractAnnotationTests {
 		assertSame(r2, secondary.get(o).get());
 	}
 
+	public void testPutRefersToResult(CacheableService<?> service) throws Exception {
+		Long id = Long.MIN_VALUE;
+		TestEntity entity = new TestEntity();
+		Cache primary = cm.getCache("primary");
+		assertNull(primary.get(id));
+		assertNull(entity.getId());
+		service.putRefersToResult(entity);
+		assertSame(entity, primary.get(id).get());
+	}
+
 	public void testMultiCacheAndEvict(CacheableService<?> service) {
 		String methodName = "multiCacheAndEvict";
 
@@ -619,6 +629,16 @@ public abstract class AbstractAnnotationTests {
 	@Test
 	public void testClassMultiPut() {
 		testMultiPut(ccs);
+	}
+
+	@Test
+	public void testPutRefersToResult() throws Exception {
+		testPutRefersToResult(cs);
+	}
+
+	@Test
+	public void testClassPutRefersToResult() throws Exception {
+		testPutRefersToResult(ccs);
 	}
 
 	@Test

spring-context/src/test/java/org/springframework/cache/config/AnnotatedClassCacheableService.java
@@ -31,6 +31,7 @@ import org.springframework.cache.annotation.Caching;
 public class AnnotatedClassCacheableService implements CacheableService<Object> {
 
 	private final AtomicLong counter = new AtomicLong();
+
 	public static final AtomicLong nullInvocations = new AtomicLong();
 
 	@Override
@@ -162,6 +163,13 @@ public class AnnotatedClassCacheableService implements CacheableService<Object> 
 	@Override
 	@Caching(put = { @CachePut("primary"), @CachePut("secondary") })
 	public Object multiUpdate(Object arg1) {
+		return arg1;
+	}
+
+	@Override
+	@CachePut(value="primary", key="#result.id")
+	public TestEntity putRefersToResult(TestEntity arg1) {
+		arg1.setId(Long.MIN_VALUE);
 		return arg1;
 	}
 }

spring-context/src/test/java/org/springframework/cache/config/CacheableService.java
@@ -70,4 +70,6 @@ public interface CacheableService<T> {
 	T multiConditionalCacheAndEvict(Object arg1);
 
 	T multiUpdate(Object arg1);
+
+	TestEntity putRefersToResult(TestEntity arg1);
 }

commit ee0e80361c22a0c88fb7a11726802776e866c869

Fix mutually exclusive use of CachePut and Cacheable
Commit eea230f introduced a regression by adding a support for the
"result" variable in SpEL expression for @CachePut. As such expressions
cannot be evaluated upfront anymore, any method that contains both
@Cacheable and @CachePut annotations are always executed even when
their conditions are mutually exclusive.

This is an example of such mutual exclusion

@Cacheable(condition = "#p1", key = "#p0")
@CachePut(condition = "!#p1", key = "#p0")
public Object getFooById(Object id, boolean flag) { ... }

This commit updates CacheEvaluationContext to define a set of
unavailable variables. When such variable is accessed for a given
expression, an exception is thrown. This is used to restore the
evaluation of the @CachePut condition upfront by registering "result"
as an unavailable variable.

If all @CachePut operations have been excluded by this upfront check,
the @Cacheable operation is processed as it was before. Such upfront
check restore the behavior prior to eea230f.

Issue: SPR-11955

spring-context/src/main/java/org/springframework/cache/interceptor/CacheAspectSupport.java
@@ -17,6 +17,7 @@
 package org.springframework.cache.interceptor;
 
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.LinkedList;
@@ -35,6 +36,7 @@ import org.springframework.cache.support.SimpleValueWrapper;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.expression.EvaluationContext;
+import org.springframework.expression.spel.SpelEvaluationException;
 import org.springframework.util.Assert;
 import org.springframework.util.ClassUtils;
 import org.springframework.util.CollectionUtils;
@@ -322,7 +324,7 @@ public abstract class CacheAspectSupport extends AbstractCacheInvoker
 		Cache.ValueWrapper result = null;
 
 		// If there are no put requests, just use the cache hit
-		if (cachePutRequests.isEmpty() && contexts.get(CachePutOperation.class).isEmpty()) {
+		if (cachePutRequests.isEmpty() && !hasCachePut(contexts)) {
 			result = cacheHit;
 		}
 
@@ -343,6 +345,27 @@ public abstract class CacheAspectSupport extends AbstractCacheInvoker
 		processCacheEvicts(contexts.get(CacheEvictOperation.class), false, result.get());
 
 		return result.get();
+	}
+
+	private boolean hasCachePut(CacheOperationContexts contexts) {
+		// Evaluate the conditions *without* the result object because we don't have it yet.
+		Collection<CacheOperationContext> cachePutContexts = contexts.get(CachePutOperation.class);
+		Collection<CacheOperationContext> excluded = new ArrayList<CacheOperationContext>();
+		for (CacheOperationContext context : cachePutContexts) {
+			try {
+				if (!context.isConditionPassing(ExpressionEvaluator.RESULT_UNAVAILABLE)) {
+	                excluded.add(context);
+				}
+			}
+			catch (SpelEvaluationException e) {
+				// Ignoring failure due to missing result, consider the cache put has
+				// to proceed
+			}
+		}
+		// check if  all puts have been excluded by condition
+		return cachePutContexts.size() != excluded.size();
+
+
 	}
 
 	private void processCacheEvicts(Collection<CacheOperationContext> contexts, boolean beforeInvocation, Object result) {

spring-context/src/main/java/org/springframework/cache/interceptor/CacheEvaluationContext.java
@@ -17,17 +17,26 @@
 package org.springframework.cache.interceptor;
 
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.aop.support.AopUtils;
 import org.springframework.core.ParameterNameDiscoverer;
+import org.springframework.expression.spel.SpelEvaluationException;
+import org.springframework.expression.spel.SpelMessage;
 import org.springframework.expression.spel.support.StandardEvaluationContext;
 import org.springframework.util.ObjectUtils;
 
 /**
- * Evaluation context class that adds a method parameters as SpEL
+ * Cache specific evaluation context that adds a method parameters as SpEL
  * variables, in a lazy manner. The lazy nature eliminates unneeded
  * parsing of classes byte code for parameter discovery.
+ *
+ * <p>Also define a set of "unavailable variables" (i.e. variables that should
+ * lead to an exception right the way when they are accessed). This can be useful
+ * to verify a condition does not match even when not all potential variables
+ * are present.
  *
  * <p>To limit the creation of objects, an ugly constructor is used
  * (rather then a dedicated 'closure'-like class for deferred execution).
@@ -36,7 +45,7 @@ import org.springframework.util.ObjectUtils;
  * @author Stephane Nicoll
  * @since 3.1
  */
-class LazyParamAwareEvaluationContext extends StandardEvaluationContext {
+class CacheEvaluationContext extends StandardEvaluationContext {
 
 	private final ParameterNameDiscoverer paramDiscoverer;
 
@@ -48,10 +57,12 @@ class LazyParamAwareEvaluationContext extends StandardEvaluationContext {
 
 	private final Map<MethodCacheKey, Method> methodCache;
 
+	private final List<String> unavailableVariables;
+
 	private boolean paramLoaded = false;
 
 
-	LazyParamAwareEvaluationContext(Object rootObject, ParameterNameDiscoverer paramDiscoverer, Method method,
+	CacheEvaluationContext(Object rootObject, ParameterNameDiscoverer paramDiscoverer, Method method,
 			Object[] args, Class<?> targetClass, Map<MethodCacheKey, Method> methodCache) {
 		super(rootObject);
 
@@ -60,6 +71,18 @@ class LazyParamAwareEvaluationContext extends StandardEvaluationContext {
 		this.args = args;
 		this.targetClass = targetClass;
 		this.methodCache = methodCache;
+		this.unavailableVariables = new ArrayList<String>();
+	}
+
+	/**
+	 * Add the specified variable name as unavailable for that context. Any expression trying
+	 * to access this variable should lead to an exception.
+	 * <p>This permits the validation of expressions that could potentially a variable even
+	 * when such variable isn't available yet. Any expression trying to use that variable should
+	 * therefore fail to evaluate.
+	 */
+	public void addUnavailableVariable(String name) {
+		this.unavailableVariables.add(name);
 	}
 
 
@@ -68,6 +91,10 @@ class LazyParamAwareEvaluationContext extends StandardEvaluationContext {
 	 */
 	@Override
 	public Object lookupVariable(String name) {
+		if (this.unavailableVariables.contains(name)) {
+			throw new SpelEvaluationException(SpelMessage.PROPERTY_OR_FIELD_NOT_READABLE,
+					name, getRootObject().getValue());
+		}
 		Object variable = super.lookupVariable(name);
 		if (variable != null) {
 			return variable;

spring-context/src/main/java/org/springframework/cache/interceptor/ExpressionEvaluator.java
@@ -44,7 +44,15 @@ import org.springframework.util.ObjectUtils;
  */
 class ExpressionEvaluator {
 
+	/**
+	 * Indicate that there is no result variable.
+	 */
 	public static final Object NO_RESULT = new Object();
+
+	/**
+	 * Indicate that the result variable cannot be used at all.
+	 */
+	public static final Object RESULT_UNAVAILABLE = new Object();
 
 
 	private final SpelExpressionParser parser = new SpelExpressionParser();
@@ -91,9 +99,11 @@ class ExpressionEvaluator {
 			final Object result) {
 		CacheExpressionRootObject rootObject = new CacheExpressionRootObject(caches,
 				method, args, target, targetClass);
-		LazyParamAwareEvaluationContext evaluationContext = new LazyParamAwareEvaluationContext(rootObject,
+		CacheEvaluationContext evaluationContext = new CacheEvaluationContext(rootObject,
 				this.paramNameDiscoverer, method, args, targetClass, this.targetMethodCache);
-		if(result != NO_RESULT) {
+		if (result == RESULT_UNAVAILABLE) {
+			evaluationContext.addUnavailableVariable("result");
+		} else if (result != NO_RESULT) {
 			evaluationContext.setVariable("result", result);
 		}
 		return evaluationContext;

spring-context/src/test/java/org/springframework/cache/interceptor/CachePutEvaluationTests.java
@@ -1,0 +1,146 @@
+/*
+ * Copyright 2002-2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cache.interceptor;
+
+import static org.junit.Assert.*;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Tests corner case of using {@link Cacheable} and  {@link CachePut} on the
+ * same operation.
+ *
+ * @author Stephane Nicoll
+ */
+public class CachePutEvaluationTests {
+
+	private ConfigurableApplicationContext context;
+
+	private Cache cache;
+
+	private SimpleService service;
+
+	@Before
+	public void setup() {
+		this.context = new AnnotationConfigApplicationContext(Config.class);
+		this.cache = context.getBean(CacheManager.class).getCache("test");
+		this.service = context.getBean(SimpleService.class);
+	}
+
+	@After
+	public void close() {
+		if (this.context != null) {
+			this.context.close();
+		}
+	}
+
+	@Test
+	public void mutualGetPutExclusion() {
+		String key = "1";
+
+		Long first = service.getOrPut(key, true);
+		Long second = service.getOrPut(key, true);
+		assertSame(first, second);
+
+		// This forces the method to be executed again
+		Long expected = first + 1;
+		Long third = service.getOrPut(key, false);
+		assertEquals(expected, third);
+
+		Long fourth = service.getOrPut(key, true);
+		assertSame(third, fourth);
+	}
+
+	@Test
+	public void getAndPut() {
+		cache.clear();
+
+		long key = 1;
+		Long value = service.getAndPut(key);
+
+		assertEquals("Wrong value for @Cacheable key", value, cache.get(key).get());
+		assertEquals("Wrong value for @CachePut key", value, cache.get(value + 100).get()); // See @CachePut
+
+		// CachePut forced a method call
+		Long anotherValue = service.getAndPut(key);
+		assertNotSame(value, anotherValue);
+		// NOTE: while you might expect the key to have been updated, it hasn't. @Cacheable operations
+		// are only processed in case of a cache miss. This is why combining @Cacheable with @CachePut
+		// is a very bad idea. We could refine the condition now that we can figure out if we are going
+		// to invoke the method anyway but that brings a whole new set of potential regression.
+		//assertEquals("Wrong value for @Cacheable key", anotherValue, cache.get(key).get());
+		assertEquals("Wrong value for @CachePut key", anotherValue, cache.get(anotherValue + 100).get());
+	}
+
+	@Configuration
+	@EnableCaching
+	static class Config extends CachingConfigurerSupport {
+
+		@Bean
+		@Override
+		public CacheManager cacheManager() {
+			return new ConcurrentMapCacheManager();
+		}
+
+		@Bean
+		public SimpleService simpleService() {
+			return new SimpleService();
+		}
+
+	}
+
+	@CacheConfig(cacheNames = "test")
+	public static class SimpleService {
+		private AtomicLong counter = new AtomicLong();
+
+		/**
+		 * Represent a mutual exclusion use case. The boolean flag exclude one of the two operation.
+		 */
+		@Cacheable(condition = "#p1", key = "#p0")
+		@CachePut(condition = "!#p1", key = "#p0")
+		public Long getOrPut(Object id, boolean flag) {
+			return counter.getAndIncrement();
+		}
+
+		/**
+		 * Represent an invalid use case. If the result of the operation is non null, then we put
+		 * the value with a different key. This forces the method to be executed every time.
+		 */
+		@Cacheable
+		@CachePut(key = "#result + 100", condition = "#result != null")
+		public Long getAndPut(long id) {
+			return counter.getAndIncrement();
+		}
+	}
+}

spring-context/src/test/java/org/springframework/cache/interceptor/ExpressionEvaluatorTests.java
@@ -29,6 +29,7 @@ import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cache.annotation.Caching;
 import org.springframework.cache.concurrent.ConcurrentMapCache;
 import org.springframework.expression.EvaluationContext;
+import org.springframework.expression.spel.SpelEvaluationException;
 import org.springframework.expression.spel.standard.SpelExpressionParser;
 import org.springframework.util.ReflectionUtils;
 
@@ -111,6 +112,14 @@ public class ExpressionEvaluatorTests {
 		EvaluationContext context = createEvaluationContext(ExpressionEvaluator.NO_RESULT);
 		Object value = new SpelExpressionParser().parseExpression("#result").getValue(context);
 		assertThat(value, nullValue());
+	}
+
+	@Test
+	public void unavailableReturnValue() throws Exception {
+		EvaluationContext context = createEvaluationContext(ExpressionEvaluator.RESULT_UNAVAILABLE);
+		thrown.expect(SpelEvaluationException.class);
+		thrown.expectMessage("'result'");
+		new SpelExpressionParser().parseExpression("#result").getValue(context);
 	}
 
 	private EvaluationContext createEvaluationContext(Object result) {