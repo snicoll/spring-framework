4fd3e71c01012dedf9592d3bbf8201a2875c9100
Stephane Nicoll <snicoll@vmware.com>
2023-08-23 11:11:33 +0200

    Polish "Support element arrays with MergedAnnotations.from"
    See gh-23344

--- spring-core/src/main/java/org/springframework/core/annotation/AnnotationsScanner.java
+++ spring-core/src/main/java/org/springframework/core/annotation/AnnotationsScanner.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2022 the original author or authors.
+ * Copyright 2002-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -84,7 +84,7 @@ abstract class AnnotationsScanner {
 	}
 
 	/**
-	 * Scan the the specified elements using {@link SearchStrategy#DIRECT} for
+	 * Scan the specified elements using {@link SearchStrategy#DIRECT} for
 	 * relevant annotations and call the processor as required.
 	 * @param context an optional context object that will be passed back to the
 	 * processor
--- spring-core/src/main/java/org/springframework/core/annotation/MergedAnnotations.java
+++ spring-core/src/main/java/org/springframework/core/annotation/MergedAnnotations.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2022 the original author or authors.
+ * Copyright 2002-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -346,7 +346,7 @@ public interface MergedAnnotations extends Iterable<MergedAnnotation<Annotation>
 	 * element annotations
 	 * @see #search(SearchStrategy)
 	 */
-	static MergedAnnotations from(AnnotatedElement element, SearchStrategy searchStrategy,
+	static MergedAnnotations from(@Nullable AnnotatedElement element, SearchStrategy searchStrategy,
 			RepeatableContainers repeatableContainers) {
 
 		return from(element, searchStrategy, repeatableContainers, AnnotationFilter.PLAIN);
