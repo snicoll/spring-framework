7a869ef406fae7d28efacd97dcdaef89f0a27e81
Stéphane Nicoll <stephane.nicoll@broadcom.com>
2024-01-05 09:03:39 +0100

    Add sample reproducing the issue

---
+++ spring-test/src/test/java/example/RequestScopedBeansOfTypeTests.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright 2002-2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package example;
+
+import java.util.function.Supplier;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.ResolvableType;
+import org.springframework.web.context.annotation.RequestScope;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RequestScopedBeansOfTypeTests {
+
+	@Test
+	void requestScopedGenericSupplier() {
+		ResolvableType type = ResolvableType.forClassWithGenerics(Supplier.class, String.class);
+		assertBeansAreFound(GenericSupplierConfiguration.class, type);
+	}
+
+	@Test
+	void requestScopedCustomSupplier() {
+		ResolvableType type = ResolvableType.forClass(CustomSupplier.class);
+		assertBeansAreFound(CustomSupplierConfiguration.class, type);
+	}
+
+	void assertBeansAreFound(Class<?> config, ResolvableType type) {
+		try (AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext()) {
+			context.register(config);
+			context.refresh();
+			ConfigurableListableBeanFactory beanFactory = context.getBeanFactory();
+			String[] names = beanFactory.getBeanNamesForType(type, true, false);
+			assertThat(names).containsExactlyInAnyOrder("scopedTarget.requestScopedBean", "requestScopedBean", "bean");
+		}
+	}
+
+	@Configuration(proxyBeanMethods = false)
+	static class GenericSupplierConfiguration {
+
+		@Bean
+		@RequestScope
+		Supplier<String> requestScopedBean() {
+			return () -> "value";
+		}
+
+		@Bean
+		Supplier<String> bean() {
+			return () -> "value";
+		}
+
+	}
+
+	@Configuration(proxyBeanMethods = false)
+	static class CustomSupplierConfiguration {
+
+		@Bean
+		@RequestScope
+		CustomSupplier requestScopedBean() {
+			return () -> "value";
+		}
+
+		@Bean
+		CustomSupplier bean() {
+			return () -> "value";
+		}
+
+	}
+
+	interface CustomSupplier extends Supplier<String> {
+
+	}
+
+}
+
